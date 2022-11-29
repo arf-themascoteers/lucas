b27a4ebe8f772ea791b0ad588bfbd1ee28df4753
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-11-29 15:36:43 +1100

    done

--- lucas_dataset.py
+++ lucas_dataset.py
@@ -12,10 +12,10 @@ import dwt
 class LucasDataset(Dataset):
     def __init__(self, is_train=True):
         self.preload = False
-        self.dump = True
+        self.dump = False
         self.DWT = False
         self.is_train = is_train
-        self.csv_file_location = "data/lucas.csv"
+        self.csv_file_location = "data/minmin.csv"
         self.work_csv_file_location_train = "data/train.csv"
         self.work_csv_file_location_test = "data/test.csv"
         self.scaler = None
@@ -27,7 +27,6 @@ class LucasDataset(Dataset):
             self.df = self._preprocess(self.df)
         else:
             self.df = pd.read_csv(self.csv_file_location)
-            self.df = self.df.drop(columns=["lc1","lu1"])
             #self.df = self.df.loc[self.df['oc'] <= 40]
             train, test = model_selection.train_test_split(self.df, test_size=0.2)
             self.df = train
@@ -43,35 +42,17 @@ class LucasDataset(Dataset):
                     self.df.to_csv(self.work_csv_file_location_test, index=False)
 
         if self.DWT:
-            self.x = dwt.transform(self.df[self.df.columns[11:]].values)
+            self.x = dwt.transform(self.df[self.df.columns[1:]].values)
         else:
-            self.x = self.df[self.df.columns[11:]].values
-            #self.x = self.df[self.df.columns[[2560,2850,1580,2844,450,1761,3508,1068,714,674,868,1823,2135,1216,3215,1133,1973,1317,2232,2956,1101]]].values
-
-        self.aux = self.df[self.df.columns[2:11]].values
-        self.y = self.df[self.df.columns[1]].values
+            self.x = self.df[self.df.columns[1:]].values
+        self.y = self.df[self.df.columns[0]].values
 
     def _preprocess(self, df):
         df = self.__scale__(df)
         return df
 
     def __scale__(self, df):
-        for col in df.columns[1:11]:
-            if col == "lc1" or col == "lu1":
-                continue
-            df, x_scaler = self.__scale_col__(df, col)
-            if col == "oc":
-                self.scaler = x_scaler
-
-        # x = df[df.columns[11:]].values.astype(float)
-        # nrows = x.shape[0]
-        # ncols = x.shape[1]
-        # x = x.reshape((nrows * ncols, 1))
-        # scaler = MinMaxScaler()
-        # x_scaled = scaler.fit_transform(x)
-        # x_scaled = x_scaled.reshape((nrows, ncols))
-        # df[df.columns[11:]] = x_scaled
-
+        df, self.scaler = self.__scale_col__(df, "soc")
         return df
 
     def __scale_col__(self, df, col):
@@ -89,19 +70,14 @@ class LucasDataset(Dataset):
 
     def __getitem__(self, idx):
         this_x = self.x[idx]
-        this_aux = self.aux[idx]
         soc = self.y[idx]
-        return torch.tensor(this_x, dtype=torch.float32), torch.tensor(this_aux, dtype=torch.float32),\
-               torch.tensor(soc, dtype=torch.float32)
+        return torch.tensor(this_x, dtype=torch.float32), torch.tensor(soc, dtype=torch.float32)
 
     def get_x(self):
         return self.x
 
     def get_y(self):
         return self.y
-
-    def get_aux(self):
-        return self.aux
 
 
 if __name__ == "__main__":
--- lucas_machine.py
+++ lucas_machine.py
@@ -6,33 +6,15 @@ import torch
 class LucasMachine(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.band_net = nn.Sequential(
-        #     nn.Linear(4200, 100)
-        #     ,
-        #     nn.ReLU(),
-        #     nn.Linear(100,9)
-        # )
-        self.band_net = nn.Sequential(
-            nn.Conv1d(1,4,10),
-            nn.LeakyReLU(),
-            nn.AvgPool1d(kernel_size=5),
-            nn.Conv1d(4,16,10),
-            nn.LeakyReLU(),
-            nn.AvgPool1d(kernel_size=5),
-            nn.Conv1d(16, 32, 10),
-            nn.LeakyReLU(),
-            nn.AvgPool1d(kernel_size=5),
-            nn.Flatten(),
-            nn.Linear(992,1)
-        )
         self.fc = nn.Sequential(
-            nn.Linear(4200, 1)
+            nn.Linear(33, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, 1)
         )
 
-    def forward(self, x, aux):
-        #x = x.reshape(x.shape[0],1,x.shape[1])
-        #x = self.band_net(x)
-        #x = torch.cat((x,aux), dim=1)
+    def forward(self, x):
         x = self.fc(x)
         return x
 
