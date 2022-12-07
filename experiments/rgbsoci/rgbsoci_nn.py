500f0e5306ea9d6dba5f91f43899bfaeb1037531
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-12-08 01:10:51 +1100

    done

---
+++ experiments/rgbsoci/rgbsoci_nn.py
@@ -0,0 +1,23 @@
+import ds_manager
+import torch
+import train
+import test
+import os
+
+
+def run_plz():
+    os.chdir("../../")
+    dm = ds_manager.DSManager(si=["soci"], ctype="rgb")
+    train_ds = dm.get_train_ds()
+    test_ds = dm.get_test_ds()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = train.train(device, train_ds)
+    r2 = test.test(device, test_ds, model)
+
+    print(r2)
+
+
+if __name__ == "__main__":
+    run_plz()
